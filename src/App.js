3a1985b1e3b29f404870fe9492b6b33defb1d4a4
Gleb Sapegin <glebsapegin@mail.ru>
2020-05-27 15:20:35 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={classes.grid}>
+        <div className={classes.subgrid1}>
+          <div className={classes.subgrid1_itemA + ' ' + classes.subgrid_item}>A</div>
+          <div className={classes.subgrid1_itemB + ' ' + classes.subgrid_item}>B</div>
+          <div className={classes.subgrid1_itemC + ' ' + classes.subgrid_item}>C</div>
+          <div className={classes.subgrid1_itemD + ' ' + classes.subgrid_item}>D</div>
+          <div className={classes.subgrid1_itemE + ' ' + classes.subgrid_item}>E</div>
+        </div>
+        <div className={classes.subgrid2}>
+          <div className={classes.subgrid2_itemF + ' ' + classes.subgrid_item}>F</div>
+        </div>
+        <div className={classes.subgrid3}>
+          <div className={classes.subgrid3_itemG + ' ' + classes.subgrid_item}>G</div>
+          <div className={classes.subgrid3_itemH + ' ' + classes.subgrid_item}>H</div>
+        </div>
+      </div>
   );
 }
 
